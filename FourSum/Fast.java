c386cc3fe4cb666894fab10945ce596ecba7b743
Emily <31772119+LPL2211@users.noreply.github.com>
2018-02-19 20:26:22 +0100

    Create Fast.java

---
+++ FourSum/Fast.java
@@ -0,0 +1,6 @@
+// Implement Binary Search for finding 4 numbers in an array which sum up to 0
+// Inspired codes here: 
+// https://www.programcreek.com/2013/02/leetcode-4sum-java/
+// https://stackoverflow.com/questions/11216582/4sum-implementation-in-java-from-leetcode
+// https://www.geeksforgeeks.org/find-four-elements-that-sum-to-a-given-value-set-2/
+// https://www.geeksforgeeks.org/find-four-numbers-with-sum-equal-to-given-sum/
