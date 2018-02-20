d03d92e396501cc4263b8ee8807e3fdc15a3836d
Satwik Kansal <satwikkansal@gmail.com>
2018-02-20 11:27:03 +0530

    Fix failing test cases

--- FourSum/Simple.java
+++ FourSum/Simple.java
@@ -15,17 +15,22 @@ public class FourSum {
         int n = Integer.parseInt(S.nextLine()); // Provided by teacher, don't change
         long[] a = new long[n]; // Provided by teacher, don't change
 
+        for (int i = 0; i < n; i++)
+        {
+            a[i] = S.nextLong();
+        }
+
         if ( a.length < 4) {
             System.out.println("False");
         } else {
             Arrays.sort(a);
-            for (int i = 0; i < n; i++) {
-                for (int j = i + 1; j < n; j++) {
-                    for (int k = j + 1; k < n; k++) {
+            for (int i = 0; i < n-3; i++) {
+                for (int j = i + 1; j < n-2; j++) {
+                    for (int k = j + 1; k < n-1; k++) {
                         for (int l = k + 1; l < n; l++) {
                             long sum = a[i] + a[j] + a[k] + a[l];
                             if (sum == 0) {
-                                System.err.println(i + " " + j + " " + k + " " + l); // Provided by teacher, don't change
+                                System.err.println(a[i] + " " + a[j] + " " + a[k] + " " + a[l]); // Provided by teacher, don't change
                                 System.out.println("True"); // Provided by teacher, don't change
                                 System.exit(0); // Provided by teacher, don't change
                             }
