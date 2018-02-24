5ad85d2cf5de96c2cc718fb0a711502ffbea000a
Satwik Kansal <satwikkansal@gmail.com>
2018-02-25 00:00:43 +0530

    * Fix the constant issue, use the algorithm from wikipedia * Use stdin instead of reading from file

--- congress/congress.py
+++ congress/congress.py
@@ -1,27 +1,38 @@
-import heapq
 from collections import OrderedDict
+import heapq
+import math
 
 TESTFILE = "1990-in.txt"
 
-# this constant is not specified in the report and I can't find it anywhere
-# so for now, I took it as 10,000. But we won't get exact answers until we
-# figure out what it should be.
-constant = 10000
-
 file_contents = []
 
+# Uncomment this if you want to read from a file
+"""
 with open(TESTFILE, 'r') as f:
     file_contents = f.readlines()
 
 num_states = int(file_contents[0])
 num_seats = int(file_contents[1])
+"""
+
+num_states = int(input())
+num_seats = int(input())
 
 heap = []
 allocation = {}
 
+# To read from a file, uncomment this
+"""
 for i in range(1, num_states):
     state_name = file_contents[2 * i].strip()
     state_population = int(file_contents[2 * i + 1])
+"""
+
+for i in range(num_states):
+    state_name = input().strip()
+    state_population = int(input())
+
+    state_population = state_population/math.sqrt(2)
 
     # python only has min-heap implementation, but our
     # use case require max-heap, hence the -ve  sign in
@@ -30,7 +41,8 @@ for i in range(1, num_states):
     # the value is state_name.
     heap.append((-state_population, state_name))
     # every state is allocated at least one seat
-    allocation[state_name] = 1
+    # allocation[state_name] = 1
+    allocation[state_name] = {"seats": 1, "population": state_population}
 
 # this function heapifies our python list `heap` to form
 # priority queue
@@ -41,19 +53,19 @@ heapq.heapify(heap)
 seats_left = num_seats - num_states
 
 for _ in range(seats_left):
-    seats, state = heapq.heappop(heap)
-    seats = -seats
+    population, state = heapq.heappop(heap)
 
-    allocation[state] += 1
+    allocation[state]["seats"] += 1
+    current_seats = allocation[state]["seats"]
 
-    # subtract the seats by constant, and push back into the heap
-    seats -= constant
-    heapq.heappush(heap, (-seats, state))
+    population = allocation[state]["population"]/(math.sqrt(current_seats * (current_seats + 1)))
+
+    heapq.heappush(heap, (-population, state))
 
 # dictionary can't be sorted in Python, so OrderedDict comes to
 # our rescue. It's not necessary but this will allow us print
 # results in sorted order
 sorted_allocation = OrderedDict(sorted(allocation.items()))
 
-for state, num_seats in sorted_allocation.items():
-    print(state, num_seats)
+for state, state_info in sorted_allocation.items():
+    print(state, state_info["seats"])
