a860d5b5ea2beb514e909383ef7073ed4e55f5fb
Satwik Kansal <satwikkansal@gmail.com>
2018-03-25 01:43:06 +0530

    Few tests passing, finally!

--- HP/hashpipe.py
+++ HP/hashpipe.py
@@ -3,12 +3,11 @@ from algs4.fundamentals.java_helper import java_string_hash
 
 class Pipe:
 
-    def __init__(self, key, val, height):
+    def __init__(self, key, val, height, predecessor=None):
         self.key = key
         self.value = val
         self.height = height
-        # store the idx of the highest box referencing a key
-        self._top_reference = -1
+        self.previous_pipe = predecessor
         self._boxes = [None] * height
 
     @property
@@ -27,8 +26,8 @@ class Pipe:
             if pipe:
                 res.append(pipe.key)
             else:
-                res.append(None)
-        return str(res)
+                res.append(".")
+        return " ".join(res)
 
     @property
     def top_reference(self):
@@ -41,69 +40,115 @@ class Pipe:
     def next_pipe(self):
         return self._boxes[0]
 
+    def key_at_height(self, height):
+        return self._boxes[height].key if height<=self.top_reference else None
+
 
 class HashPipe:
 
     def __init__(self):
-        self.root = Pipe(-float('inf'), None, 32)
+        self.root = Pipe(-float('inf'), "ROOT", 32, None)
 
     def size(self):
         pass
 
     def put(self, key, val):
+        if self.exists(key):
+            self.lookup(self.root, key).value = val
+            return
+
         predecessor = self.floor(self.root, key)
         height = calculate_pipe_height(key)
         new_pipe = Pipe(key, val, height)
+        new_pipe.previous_pipe = predecessor
 
-        if predecessor is None:
+        # print("predecessor is {}".format(predecessor.key))
+
+        # min element till now
+        if predecessor.value is "ROOT":
             for i in range(height):
                 old_ref = self.root._boxes[i]
                 self.root._boxes[i] = new_pipe
                 new_pipe._boxes[i] = old_ref
-            new_pipe._top_reference = height - 1
-            self.root._top_reference = max(self.root.top_reference, height - 1)
             return
 
+        # key already exists
         if predecessor.key == key:
             predecessor.value = val
             return
 
+        # otherwise, update the references
         predecessor_height = predecessor.height
+        next_pipe = predecessor.next_pipe
 
+        if next_pipe:
+            next_pipe.previous_pipe = new_pipe
+
+        # fit upto the common height
         for i in range(min(height, predecessor_height)):
             old_ref = predecessor._boxes[i]
             predecessor._boxes[i] = new_pipe
             new_pipe._boxes[i] = old_ref
 
-        while height > predecessor_height:
-            next_pipe = predecessor.next_pipe
+        # if our new pipe is taller than the predecessor
+        # we need to add forward references
+        while height > predecessor_height and next_pipe:
             if next_pipe.height > predecessor_height:
+                # doubt
                 for i in range(predecessor_height,
                                min(next_pipe.height, height)):
                     new_pipe._boxes[i] = next_pipe
-            predecessor_height = next_pipe.height
+                predecessor_height = min(next_pipe.height, height)
+            next_pipe = next_pipe.next_pipe
 
-        new_pipe._top_reference = height - 1
+        # we also need to check backward references
+        if height > predecessor.height:
+            covered_height = predecessor.height
+            predecessor = predecessor.previous_pipe
+            while predecessor is not None and covered_height < height:
+                if predecessor.height > covered_height:
+                    for i in range(covered_height, min(predecessor.height, height)):
+                        predecessor._boxes[i] = new_pipe
+                    covered_height = min(predecessor.height, height)
+                predecessor = predecessor.previous_pipe
 
     def get(self, key):
         pipe = self.lookup(self.root, key)
         return pipe.value if pipe else None
 
+    def exists(self, key):
+        return True if self.lookup(self.root, key) else False
+
     def lookup(self, start, key):
-        if start or start.key is None:
+        if key is None:
+            # print("None key found for lookup")
             return None
 
+        # A none reference
+        if start is None or start.key is None:
+            # print("Looking for {} in none Pipe".format(key))
+            return None
+
+        # print("Lookup for {} with pipe as {}".format(key, start.key))
+
+        # Found!
         if start.key == key:
             return start
 
-        elif start.key < key:
-            self.get(start._boxes[start.top_reference], key)
+        top_ref = start.top_reference
+
+        if top_ref == -1:
+            # print("Empty pipe encountered with key {} while looking for {}".format(start.key, key))
+            pass
+
+        elif start._boxes[top_ref].key <= key:
+            return self.lookup(start._boxes[top_ref], key)
 
         else:
             refs = start._boxes
-            top_ref = start.top_reference
+            top_ref -= 1
             while top_ref >= 0:
-                res = self.get(refs[top_ref], key)
+                res = self.lookup(refs[top_ref], key)
                 if res:
                     return res
                 top_ref -= 1
@@ -112,14 +157,20 @@ class HashPipe:
 
     def floor(self, start, key):
         """
-        Returns pipe, with smallest key greater than or equal to the `key`
+        Returns pipe, with largest key smaller than or equal to the `key`
         provided.
         """
         string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
         for char in string[::-1]:
-            if char < key and self.get(key):
-                return self.lookup(self.root, key)
+            # hack
+            if char < key and self.exists(char):
+                return self.lookup(self.root, char)
+            else:
+                # print("Key {}, not found.".format(char))
+                pass
+
+        return self.root
 
     def control(self, key, height):
         """
@@ -127,24 +178,26 @@ class HashPipe:
         """
         pipe = self.lookup(self.root, key)
         if pipe:
-            return pipe._boxes[height].key
-        return None
+            return pipe.key_at_height(height)
+        else:
+            return None
 
     def create_string_to_debug(self, pipe, string_so_far):
         if pipe is None:
-            return
-        string_so_far += str(pipe) + "\n"
-        refs = pipe.boxes
+            return string_so_far
+
+        string_so_far += str(pipe) + "\n*"
+        refs = pipe._boxes
         top_ref = pipe.top_reference
+
+        # print(pipe.key, top_ref)
 
         if top_ref == -1:
             return string_so_far
 
         else:
-            height = len(refs)
-            first_occupied = height - (top_ref + 1)
-            for p in refs[first_occupied:]:
-                self.create_string_to_debug(p, string_so_far)
+            for p in refs[:top_ref + 1]:
+                string_so_far += self.create_string_to_debug(p, string_so_far)
 
         return string_so_far
 
@@ -163,10 +216,23 @@ def calculate_pipe_height(key):
     return num_trailing_zeroes + 1
 
 
-string = "SEARCHEXAMPLE"
 
-hp = HashPipe()
-
-for idx, char in enumerate(string):
-    hp.put(char, idx)
-    print(hp)
+import string
+# ex = list(string.ascii_uppercase)
+ex = list("SEARCHEXAMPLE")
+H =  HashPipe()
+j=0
+for k in ex:
+    print("Insert: ", k)
+    H.put(k, j)
+    cl = [ H.control(-float('inf'), h) for h in range(32) ]
+    #        print(cl)
+    print( " ".join(x if x else '.' for x in cl) + "  : " + "ROOT" )
+    for g in range(j+1):
+        cl = [ H.control(ex[g], h) for h in range(32) ]
+        #        print(cl)
+        pipe = H.lookup(H.root, ex[g])
+        prev = pipe.previous_pipe.key if pipe.previous_pipe else "None"
+        nxt = pipe.next_pipe.key if pipe.next_pipe else "None"
+        print( " ".join(x if x else '.' for x in cl) + "  : " + ex[g] + " {} {}".format(pipe.height, prev, nxt))
+    j += 1
